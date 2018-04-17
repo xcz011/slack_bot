d9bb4c8650d436be03f2d845f3b2feecd4ee31fa
chuanzhu xu <chuanzhu.xu@gmail.com>
2018-04-16 22:41:56 -0400

    second commit, channal id work

--- app.py
+++ app.py
@@ -16,80 +16,6 @@ slack = pyBot.client
 app = Flask(__name__)
 
 
-def _event_handler(event_type, slack_event):
-    """
-    A helper function that routes events from Slack to our Bot
-    by event type and subtype.
-
-    Parameters
-    ----------
-    event_type : str
-        type of event recieved from Slack
-    slack_event : dict
-        JSON response from a Slack reaction event
-
-    Returns
-    ----------
-    obj
-        Response object with 200 - ok or 500 - No Event Handler error
-
-    """
-    team_id = slack_event["team_id"]
-    # ================ Team Join Events =============== #
-    # When the user first joins a team, the type of event will be team_join
-    if event_type == "team_join":
-        user_id = slack_event["event"]["user"]["id"]
-        # Send the onboarding message
-        pyBot.onboarding_message(team_id, user_id)
-        return make_response("Welcome Message Sent", 200,)
-
-    # ============== Share Message Events ============= #
-    # If the user has shared the onboarding message, the event type will be
-    # message. We'll also need to check that this is a message that has been
-    # shared by looking into the attachments for "is_shared".
-    elif event_type == "message" and slack_event["event"].get("attachments"):
-        user_id = slack_event["event"].get("user")
-        if slack_event["event"]["attachments"][0].get("is_share"):
-            # Update the onboarding message and check off "Share this Message"
-            pyBot.update_share(team_id, user_id)
-            return make_response("Welcome message updates with shared message",
-                                 200,)
-
-    # ============= Reaction Added Events ============= #
-    # If the user has added an emoji reaction to the onboarding message
-    elif event_type == "reaction_added":
-        user_id = slack_event["event"]["user"]
-        # Update the onboarding message
-        pyBot.update_emoji(team_id, user_id)
-        return make_response("Welcome message updates with reactji", 200,)
-
-    # =============== Pin Added Events ================ #
-    # If the user has added an emoji reaction to the onboarding message
-    elif event_type == "pin_added":
-        user_id = slack_event["event"]["user"]
-        # Update the onboarding message
-        pyBot.update_pin(team_id, user_id)
-        return make_response("Welcome message updates with pin", 200,)
-
-    # ============= Event Type Not Found! ============= #
-    # If the event_type does not have a handler
-    message = "You have not added an event handler for the %s" % event_type
-    # Return a helpful error message
-    return make_response(message, 200, {"X-Slack-No-Retry": 1})
-
-
-@app.route("/install", methods=["GET"])
-def pre_install():
-    """This route renders the installation page with 'Add to Slack' button."""
-    # Since we've set the client ID and scope on our Bot object, we can change
-    # them more easily while we're developing our app.
-    client_id = pyBot.oauth["client_id"]
-    scope = pyBot.oauth["scope"]
-    # Our template is using the Jinja templating language to dynamically pass
-    # our client id and scope
-    return render_template("install.html", client_id=client_id, scope=scope)
-
-
 @app.route("/thanks", methods=["GET", "POST"])
 def thanks():
     """
@@ -118,7 +44,7 @@ def hears():
     # sends back.
     #       For more info: https://api.slack.com/events/url_verification
     slack_event = json.loads(request.data)
-
+    print(slack_event)
     if "challenge" in slack_event:
         return make_response(slack_event["challenge"], 200, {"content_type":
                                                              "application/json"
@@ -137,8 +63,8 @@ def hears():
     # ====== Process Incoming Events from Slack ======= #
     # If the incoming request is an Event we've subcribed to
     if "event" in slack_event:
-        webhook_url = 'https://hooks.slack.com/services/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'
-        slack_data = {'text': "this is xu testing, please ignore, thanks"}
+        # webhook_url = 'https://hooks.slack.com/services/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'
+        # slack_data = {'text': "this is xu testing, please ignore, thanks"}
 
 
         # webhook_url = 'https://slack.com/api/channels.create'
@@ -147,14 +73,18 @@ def hears():
         #               'validate': 'true'
         #             }
         
-        response = requests.post(
-            webhook_url, data=json.dumps(slack_data),
-            headers={'Content-Type': 'application/json'}
-        )
-        print(response)
-        # event_type = slack_event["event"]["type"]
-        # # Then handle the event by event_type and have your bot respond
-        # return _event_handler(event_type, slack_event)
+        # response = requests.post(
+        #     webhook_url, data=json.dumps(slack_data),
+        #     headers={'Content-Type': 'application/json'}
+        # )
+        channel_id = slack_event['event']['item']['channel']
+        print((type(slack_event)))
+        pyBot.post_message_by_channel(channel_id, 'channel id is'+ channel_id)
+        
+        
+        
+        make_response('success', 200, {"content_type":"application/json" } )
+
     # If our bot hears things that are not events we've subscribed to,
     # send a quirky but helpful error response
     return make_response("[NO EVENT IN SLACK REQUEST] These are not the droids\
